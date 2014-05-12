b9281711fe1b15d9b96d0107e09510804f930602
rgex <124898@supinfo.com>
2014-05-11 20:30:51 -0400

    first commit

---
+++ BitcoinECDSA.php
@@ -0,0 +1,152 @@
+<?php
+
+if (!extension_loaded('gmp')) {
+	throw new Exception('GMP extension seems not to be installed');
+}
+
+class BitcoinECDSA {
+
+	public function __construct() {
+		$this->a = gmp_init("0");
+		$this->b = gmp_init("7");
+		$this->p = gmp_init("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F",16);
+
+		$this->G = array('x' => gmp_init("55066263022277343669578718895168534326250603453777594175500187360389116729240"),
+			   	 'y' => gmp_init("32670510020758816978083085130507043184471273380659243275938904335757337482424"));
+	}
+
+	public function base58_permutation($number) {
+		$table = array("1","2","3","4","5","6","7","8","9","A","B","C","D",
+		"E","F","G","H","J","K","L","M","N","P","Q","R","S","T","U","V","W",
+		"X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","m","n","o",
+		"p","q","r","s","t","u","v","w","x","y","z");
+
+		return $table[$number];
+	}
+
+	//Wallet Import Format
+	public function wif($secretKey) {
+		$secretKey = "80".$secretKey;
+		$firstSha256 = hash("sha256",hex2bin($secretKey));
+		$secondSha256 = hash("sha256",hex2bin($firstSha256));
+		$secretKey .= substr($secondSha256,0,8);
+		return strrev($this->base58_encode($secretKey));
+	}
+
+	public function base58_encode($data,$base = 16) {
+		$res ="";
+		$dataIntVal = gmp_init($data,$base);
+		while(gmp_cmp($dataIntVal,"0") > 0)
+		{
+			$qr = gmp_div_qr($dataIntVal, 58);
+			$dataIntVal = $qr[0];
+			$reminder = gmp_strval($qr[1]);
+			$res .= $this->base58_permutation($reminder);
+		}
+		return $res;
+	}
+
+	public function doublePoint($pt) {
+		
+		$a = $this->a;
+		$b = $this->b;
+		$p = $this->p;
+
+
+		// SLOPE = (3 * ptX^2 + a )/( 2*ptY )
+
+		$gcd = gmp_strval(gmp_gcd(gmp_mul( 2,$pt['y'] ),$p));
+		if($gcd!="1")
+		{
+			throw new Exception('GCD is not equal to 1, this is not handled by this library');
+		}
+
+		$s = gmp_mod(gmp_mul(gmp_invert(gmp_mul( 2,$pt['y'] ),$p),gmp_add(gmp_mul(3,gmp_pow($pt['x'],2)),$a)),$p);
+
+		$nPt['x'] = gmp_mod(gmp_sub(gmp_sub(gmp_pow($s,2),$pt['x']),$pt['x']),$p);
+		$nPt['y'] = gmp_mod(gmp_sub(gmp_mul($s,gmp_sub($pt['x'],$nPt['x'])),$pt['y']),$p);
+
+		return $nPt;
+	}
+
+	public function addPoints($pt1,$pt2) {
+	
+		$a = $this->a;
+		$b = $this->b;
+		$p = $this->p;
+
+		if(gmp_cmp($pt1['x'],$pt2['x']) == 0) //if identical
+			return $this->doublePoint($pt1);
+
+
+		// SLOPE = (pt1Y - pt2Y)/( pt1X - pt2X )
+		// <=> (pt1Y - pt2Y) * ( pt1X - pt2X )^-1
+
+		$gcd = gmp_strval(gmp_gcd(gmp_sub($pt1['x'],$pt2['x']),$p));
+		if($gcd!="1")
+		{
+			throw new Exception('GCD is not equal to 1, this is not handled by this library');
+		}
+
+		$s = gmp_mod(gmp_mul(gmp_sub($pt1['y'],$pt2['y']),gmp_invert(gmp_sub($pt1['x'],$pt2['x']),$p)),$p);
+
+		$nPt['x'] = gmp_mod(gmp_sub(gmp_sub(gmp_pow($s,2),$pt1['x']),$pt2['x']),$p);
+		$nPt['y'] = gmp_mod(gmp_sub(gmp_mul($s,gmp_sub($pt1['x'],$nPt['x'])),$pt1['y']),$p);
+
+		return $nPt;
+	}
+
+	public function mulPoint($k,$pG) {
+		//in order to calculate k*G
+
+		$a = $this->a;
+		$b = $this->b;
+		$p = $this->p;
+
+		$k = gmp_init($k);
+		$kBin = gmp_strval($k, 2);
+
+		$lastPoint = $pG;
+		for($i = 1; $i < strlen($kBin); $i++) {
+		
+			if( substr($kBin,$i,1) == 1 ) {
+				$dPt = $this->doublePoint($lastPoint);
+				$lastPoint = $this->addPoints($dPt,$pG);
+			}
+			else {
+				$lastPoint = $this->doublePoint($lastPoint);
+			}
+		
+		}
+		return $lastPoint;
+	}
+
+	public function generatePubKey($k) {
+
+		$a = $this->a;
+		$b = $this->b;
+		$p = $this->p;
+		$G = $this->G;
+
+		$pubKey 	= $this->mulPoint(gmp_strval(gmp_init($k,16)),array('x'=>$G['x'],'y'=>$G['y']),$a,$b,$p);
+		$pubKey['x']	= gmp_strval($pubKey['x'],16);
+		$pubKey['y']	= gmp_strval($pubKey['y'],16);
+		return $pubKey;
+	}
+
+	public function generateAddress($pubKey) {
+		$address  	= "04".$pubKey['x'].$pubKey['y'];
+		$sha256		= hash("sha256",hex2bin($address));
+		$ripem160 	= hash("ripemd160",hex2bin($sha256));
+		$address 	= "00".$ripem160; //00 = main network, 6f = test network
+
+		//checksum
+		$sha256		= hash("sha256",hex2bin($address));
+		$sha256		= hash("sha256",hex2bin($sha256));
+		$address 	= $address.substr($sha256,0,8);
+		return strrev($this->base58_encode($address)."1");
+	}
+
+}
+
+?>
---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+require_once("BitcoinECDSA.php");
+$bitcoinECDSA = new BitcoinECDSA();
+
+$k ="0C28FCA386C7A227600B2FE50B7CAE11EC86D3BF1FBE471BE89827E19D72AA1D";
+
+print_r($bitcoinECDSA->wif($k));
+
+echo "<br/>";
+
+$pubKey = $bitcoinECDSA->generatePubKey($k);
+print_r($pubKey);
+
+echo "<br/>";
+
+print_r($bitcoinECDSA->generateAddress($pubKey));
+?>
