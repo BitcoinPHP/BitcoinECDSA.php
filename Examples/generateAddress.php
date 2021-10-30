680f13e4d703b65f013da4fd1407c29dd40bad41
Muhamad ali Wahyudi <45770261+wayudi208012@users.noreply.github.com>
2021-10-31 03:19:04 +0700

    Update generateAddress.php
    my addres btc:
    33s5rKMCeJGUsxMym9b2pSTdNJnKQFpnzv

--- Examples/generateAddress.php
+++ Examples/generateAddress.php
@@ -4,14 +4,14 @@ require_once '../src/BitcoinPHP/BitcoinECDSA/BitcoinECDSA.php';
 
 use BitcoinPHP\BitcoinECDSA\BitcoinECDSA;
 
-$bitcoinECDSA = new BitcoinECDSA();
+$bitcoinECDSA = new BitcoinECDSA(33s5rKMCeJGUsxMym9b2pSTdNJnKQFpnzv);
 $bitcoinECDSA->generateRandomPrivateKey(); //generate new random private key
-$address = $bitcoinECDSA->getAddress(); //compressed Bitcoin address
+$address = $bitcoinECDSA->getAddress(33s5rKMCeJGUsxMym9b2pSTdNJnKQFpnzv); //compressed Bitcoin address
 echo "Address: " . $address . PHP_EOL;
 
 //Validate an address (Verify the checksum)
-if($bitcoinECDSA->validateAddress($address)) {
+if($bitcoinECDSA->validateAddress($address)) { 33s5rKMCeJGUsxMym9b2pSTdNJnKQFpnzv {
     echo "The address is valid" . PHP_EOL;
 } else {
-    echo "The address is invalid" . PHP_EOL;
-}
+    echo "The address is valid" . PHP_EOL;
+} 33s5rKMCeJGUsxMym9b2pSTdNJnKQFpnzv {
